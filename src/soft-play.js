323810fa47bd59ba6234bacc5a583c37782386ce
Kiran Gurung <kg820532@gmail.com>
2021-12-16 17:47:24 +0000

    we attempted all exercise, failed all but we atleast broke down the code an spoke about different ways to tackle the problem

--- src/soft-play.js
+++ src/soft-play.js
@@ -10,29 +10,35 @@ let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
-let counter = 0
+
+/* EXERCISE 1 */
 function entry(numChildren, numAdults) {
   adults = numAdults + adults
-  children = numChildren + children 
+  children = numChildren + children
   if (children > adults) {
     return false
   } else return true
 }
 
-function exit(numChildren, numAdults){
+/* EXERCISE 2 */
+function exit(numChildren, numAdults) {
   adults = numAdults - adults
   children = numChildren - children
-  if (numChildren < numAdults){
+  if (numChildren < numAdults || numAdults < numChildren) {
     return false
-  } 
-    if (numAdults < numChildren)
-    { return false}
-  if ( numAdults > adults || numChildren > children)
-  {return false}
-} 
+  }
+  if (numAdults > adults || numChildren > children) { return false }
+}
 
-function occupancy ( numChildren, numAdults){
-  
+/* EXERCISE 3 */
+function occupancy(numChildren, numAdults) 
+{
+let count = {
+  'adults' : 0,
+  'children' : 0
+} 
+numChildren = count.children
+numAdults = count.adults
 }
 
 // TODO: Change the undefined values below to the name of your functions
